48961095e61925e797a4cc8c81eb1794101642a3
Zogoury <david.zogoury125@gmail.com>
2023-02-20 16:16:19 +0100

    news

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import Auth from './src/components/screens/Auth/Auth';
 
 export default function App() {
+  let user = {
+    name: 'John',
+    age: 30,
+  };
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Button title="Click Me" onPress={() => alert('Button Clicked!')} />
+      {user === null ? <Auth /> : <Text>Welcome {user.name}</Text>}
     
       <StatusBar style="auto" />
     </View>
